f45bbb4a5249a2c1f2cd2c9f2f59028e673b2ba9
5-315-27\cic <kcwon116@naver.com>
2026-04-29 10:43:36 +0900

    feat: 함수화 완료

--- python.py
+++ python.py
@@ -1,10 +1,17 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
 
 a=int(input("a:"))
 b=int(input("b:"))
 op=int("op:")
+
 if op =='+': print(add(a,b))
-if op =='-': print(a-b)
-if op =='*': print(a*b)
-if op =='/': print(a/b)
+elif op == '-': print(sub(a,b))
+elif op == '*': print(mul(a,b))
+elif op =='/': print(div(a,b))
