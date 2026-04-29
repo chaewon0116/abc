cffc0298deb08d7e1a3203a25806c377fcf2ace5
5-315-27\cic <kcwon116@naver.com>
2026-04-29 10:40:52 +0900

    feat: ADD 함수화

--- python.py
+++ python.py
@@ -1,7 +1,10 @@
+def add(a,b):
+    return a+b
+
 a=int(input("a:"))
 b=int(input("b:"))
 op=int("op:")
-if op =='+': print(a+b)
+if op =='+': print(add(a,b))
 if op =='-': print(a-b)
 if op =='*': print(a*b)
 if op =='/': print(a/b)
